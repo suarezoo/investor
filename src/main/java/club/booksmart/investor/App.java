634501de0e6d1403d3d0081fb6d6909d157880aa
suarezoo <regproduct@yahoo.com>
2015-03-03 08:24:27 -0800

    Hello World with maven
    Adding Hello World maven app to setup basic directory structure

---
+++ src/main/java/club/booksmart/investor/App.java
@@ -0,0 +1,13 @@
+package club.booksmart.investor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
